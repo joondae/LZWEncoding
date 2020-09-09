418064dc8e1261c32fc44b48bf88fa8ff38508cd
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2020-09-09 14:14:05 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -2,44 +2,32 @@ import java.util.*;
 import java.io.*;
 public class Encoder{
 	private static String fileInput = null;
-	private static double maxSize; //Max Table size is based on the bit length input.
-	private static String LZWFileName;
+	private static String fileName;
 	
+	public static void Encode_string(String inputString) throws IOException {
 
-	/** Compress a string to a list of output symbols and then pass it for compress file creation.
-	 * @param input_string //Filename that is used for encoding. */
-	
-	public static void Encode_string(String input_string) throws IOException {
-	
-		maxSize = 69;	
-			
-		double tableSize =  255;
-		
 		HashMap<String, Integer> table = new HashMap<String, Integer>();
-		
+		int tableSize=255;
 		for (int i = 0; i < 255 ; i++){
-			table.put("" + (char) i, i);
+			table.put("" + (char) i, i); //pre-fills the hashmap with ascii characters
 		}
 
-		String initString = "";
-		
-		ArrayList<Integer> encodedValues = new ArrayList<Integer>();
-		
-		for (char symbol : inputString.toCharArray()) {
-			String symbol = initString + symbol;
-			if (tableSize.containsKey(symbol))
-				initString = symbol;
-			else {
-				encodedValues.add(table.get(initString));
-			
-				if(tableSize < maxSize)
-					table.put(symbol, (int) table_Size++);
-				initString = "" + symbol;
+		String temp = ""; //will be used to cycle through inputString
+		ArrayList<Integer> encodedValues = new ArrayList<Integer>(); //key of encoded characters
+
+		for (char start : inputString.toCharArray()) {
+			String symbol = temp + start;
+			if (table.containsKey(symbol)){ //checks to see if the symbol is already in the table
+				temp = symbol;
+			}else{
+				encodedValues.add(table.get(temp)); //if the symbol isn't in the table it is added
+				table.put(symbol,tableSize++);
+				temp = "" + start;
 			}
 		}
 
-		if (!initString.equals(""))
-			encodedValues.add(table.get(initString));
+		if (!temp.equals("")) //making sure the last symbol doesn't get skipped
+			encodedValues.add(table.get(temp));
 		
 		CreateLZWfile(encodedValues); 
 		
