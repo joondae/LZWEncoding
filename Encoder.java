7f5633027dc794dbc0804bbd95b0af4fee7b59a0
annelieselovespugs <annelieseardizzone@Brynhildr.local>
2020-09-11 18:14:31 -0700

    Limiting dictionary size
    On line 72, an extra piece was added to the if statement in order to properly limit the length of the dictionary:
    Before:
    				if (previousPlusCurrent.length() > 1)
    
    ---
    
    After:
    				if ((previousPlusCurrent.length() > 1) && theDictionary.size() < 256)

--- Encoder.java
+++ Encoder.java
@@ -1,44 +1,46 @@
 //Nathan Thomas and Grant Bishop
 import java.util.*;
 import java.io.*;
-public class Encoder
-{
+
+public class Encoder{
 private static String fileName;
 private ArrayList<Integer> encodedValues = new ArrayList<Integer>(); //key of encoded characters
-public void Encode (String inputString) throws IOException {
-	
-	HashMap<String, Integer> table = new HashMap<String, Integer>();
-	int max=500; //maximum hashmap size
-	int tableSize=256;
-	
-	for (int i = 0; i < 256 ; i++){
-		table.put("" + (char) i, i); //pre-fills the hashmap with ascii characters
-	}
-
-	String temp = ""; //will be used to cycle through inputString
-	
-
-	for (char start : inputString.toCharArray()) { //converts the input into a array of chars and cycles through it
-		String symbol = temp + start;
-		if (table.containsKey(symbol)){ //checks to see if the symbol is already in the table
-			temp = symbol;
-		}else{
-			encodedValues.add(table.get(temp)); //if the symbol isn't in the table it is added
-			if (tableSize<max){
-				table.put(symbol,tableSize++);
-			}
-			temp = "" + start;
+	public void Encode (String inputString) throws IOException {
+		
+		HashMap<String, Integer> table = new HashMap<String, Integer>(); //table = dictionary
+		int max=500; //maximum hashmap size
+		int tableSize=256;
+		
+		for (int i = 0; i < 256 ; i++){
+			table.put("" + (char) i, i); //pre-fills the hashmap with ascii characters
 		}
+
+		String temp = ""; //will be used to cycle through inputString
+		
+		//Reads char by char from a string given at the start (not a txt file)
+		for (char start : inputString.toCharArray()) { //converts the input into a array of chars and cycles through it
+			String symbol = temp + start;
+			if (table.containsKey(symbol)){ //checks to see if the symbol is already in the table
+				temp = symbol;
+			}
+			else{
+				encodedValues.add(table.get(temp)); //if the symbol isn't in the table it is added
+				if (tableSize<max){
+					table.put(symbol,tableSize++);
+				}
+				temp = "" + start;
+			}
+		}
+
+		if (!temp.equals("")) //making sure the last symbol doesn't get skipped
+			encodedValues.add(table.get(temp));
+
+		
+		createFile("LZW.txt");
 	}
 
-	if (!temp.equals("")) //making sure the last symbol doesn't get skipped
-		encodedValues.add(table.get(temp));
-
+	public void createFile(String inputFile) throws IOException{ //creates a new file and outputs the encoded values
 	
-	createFile("LZW.txt");
-}
-	public void createFile(String inputFile) throws IOException //creates a new file and outputs the encoded values
-	{
 		
 		BufferedWriter bw = new BufferedWriter(new FileWriter("LZW" + inputFile));
 		for(int i = 0;i<encodedValues.size();i++)
@@ -49,8 +51,7 @@ public void Encode (String inputString) throws IOException {
 		
 	}
 	
-	public static void main(String[]args) throws IOException //runs it
-	{
+	public static void main(String[]args) throws IOException{ //runs it
 		Encoder en = new Encoder();
 		BufferedReader br = new BufferedReader(new FileReader("LZW.txt"));
 		int temp;
