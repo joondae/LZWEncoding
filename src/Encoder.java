e78d6644de913c72f68f279572235ef513b68b89
joondae <68300257+joondae@users.noreply.github.com>
2020-09-24 16:01:13 -0700

    Update Encoder.java
    Cleaned up comments
    
    Optimized code so that it only reads file to encode once (works with BufferedReader)
    
    Created helper methods for better readability

--- src/Encoder.java
+++ src/Encoder.java
@@ -10,69 +10,91 @@ import java.util.Scanner;
 
 //Nathan Thomas and Grant Bishop
 //Improvements by Maya Doyle and Anneliese Ardizzone ʕ•́ᴥ•̀ʔb
-
+//Optimized by Max Hahn and Reginaldo Kim
 
 public class Encoder{
-	private static String fileName; //This variable is the name of the file.
-	private ArrayList<Integer> encodedValues = new ArrayList<Integer>(); //This variable is the key of encoded characters.
-	
+	private static String fileName;
+	private final int maxDictionarySize = 512;
 
-	//--------------------------------------------------------------------------------------------------------------------
+	//This method contains all helper methods used to encode file
+	public void encode () {
+		printStatistics(createCodestream(initializeDictionary()));
+	}
 
-	public void Encode (String inputString) throws IOException {
-		HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
-		int max=512; //maximum dictionary size
-		int dictionarySize=256; //current # of items in the dictionary
+	//This method initializes the dictionary with ASCII characters whose decimal values range from 0-255
+	private HashMap<String, Integer> initializeDictionary() {
+		HashMap<String, Integer> dictionary = new HashMap<String, Integer>(maxDictionarySize);
 		
-		//This pre-fills the hashmap with ascii characters
 		for (int i = 0; i < 256 ; i++){ 
 			dictionary.put("" + (char) i, i); 
 		}
-
-
-		//The below method reads char by char from a string given at the start to create an array list of Integers called encodedValues. It also fills the dictionary.
-		String temp = ""; //This string will be used to cycle through inputString. (This is the "previous character/string".)
-		for (char start : inputString.toCharArray()) { //This line converts the input into an array of chars and cycles through it char by char
-			String symbol = temp + start;
-			if (dictionary.containsKey(symbol)){ //This checks to see if the symbol is already in the dictionary. If it is, temp is set to symbol.
-				temp = symbol;
-			}
-			else{
-				encodedValues.add(dictionary.get(temp)); //If the symbol isn't in the dictionary, it is added.
-				if (dictionarySize < max){ 
-					dictionary.put(symbol,dictionarySize++); 
-				}
-				else{ //If the dictionary is full, the below line is printed to alert the user.
-					System.out.println ("Dictionary is full. Stopping compression.");
-				}
-				temp = "" + start;
-			}
-		}
-
-		//This ensures that the last symbol isn't skipped when encoding.
-		if (!temp.equals(""))
-			encodedValues.add(dictionary.get(temp));
-
-		//The below creates a new file called fileName
-		printCodestreamAndStatistics(fileName);
+				
+		return dictionary;
 	}
-
-
-	//--------------------------------------------------------------------------------------------------------------------
 	
-	//The below method writes each Integer in encodedValue to encodedFile.txt
+	//This method generates the list of encoded characters (according to LZW compression formula)
+	//and prints them to encodedFile.txt AND to terminal console (the latter is for debugging)
+	private ArrayList<Integer> createCodestream(HashMap<String, Integer> dictionary) {
+		ArrayList<Integer> codestream = new ArrayList<Integer>();
+		String previous = "";
+		int dictionarySize = 256;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("encodedFile.txt")));
+			
+			while(br.ready() && dictionary.size() < maxDictionarySize) {
+				char current = (char) br.read();
+				
+				//Is the string P + C present in the dictionary?
+				if(dictionary.containsKey(previous + current)) {
+					//P = concat(P,C)
+					previous += current;
+				}
+				else {
+					//output the code word which denotes P to the codestream
+					codestream.add(dictionary.get(previous));
+					bw.write(dictionary.get(previous) + " ");
+					
+					//add the string concat(P,C) to the dictionary
+					dictionary.put(previous + current, dictionarySize++);
+					
+					//P = C
+					previous = Character.toString(current);
+				}
+			}
+			
+			//accounts for last read
+			codestream.add(dictionary.get(previous));
+			bw.write(dictionary.get(previous) + "");
+			
+			//if dictionary size reaches its limit, print error message and stop compression
+			if(dictionary.size() >= maxDictionarySize) {
+				System.out.println("Maximum dictionary size reached - stopping compression.");
+				
+				while(br.ready()) {
+					int uncompressedChar = br.read();
+					codestream.add(uncompressedChar);
+					bw.write(uncompressedChar + " ");
+				}
+			}
+			
+			//ensures that no output gets "stuck" in buffer
+			br.close();
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return codestream;
+	}
+	
+	
+	//This method writes each Integer in encodedValue to encodedFile.txt
 	//and prints codestream size, original and encoded file sizes (bytes),
 	//compression ratio, and space savings
-	public void printCodestreamAndStatistics(String inputFile) throws IOException{
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("encodedFile.txt")));
-		for(int i = 0; i < encodedValues.size(); i++)
-		{
-			bw.append("" + encodedValues.get(i) + " ");
-		}
-		
-		bw.close();
-		
-		System.out.println("codstream size: " + encodedValues.size());
+	public void printStatistics(ArrayList<Integer> codestream) {
+		System.out.println("codestream size: " + codestream.size());
 		
 		File encodedFile = new File("encodedFile.txt");
 		File originalFile = new File(fileName);
@@ -81,22 +103,14 @@ public class Encoder{
 		System.out.println("\nCompression ratio: " + ((double) originalFile.length()/encodedFile.length()));
 		System.out.println("Space savings: " + (1 - ((double) encodedFile.length()/originalFile.length())));
 	}
-
-	//--------------------------------------------------------------------------------------------------------------------
 	
-	public static void main(String[]args) throws IOException{ //The below code runs the other methods.
+	//Main method
+	public static void main(String[]args) {
 		Scanner keyboard = new Scanner(System.in);
-    	System.out.print ("Enter file name/path for encoding: ");
-    	fileName = keyboard.nextLine();
-
+		System.out.print ("Enter file name/path for encoding: ");
+    	fileName = keyboard.next();
+    	
 		Encoder en = new Encoder();
-		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
-		int temp;
-		StringBuffer input = new StringBuffer();
-		while ((temp=br.read())!=-1){
-			input.append((char)temp);
-		}
-		en.Encode(input.toString());
-		
+    	en.encode();
 	}
 }
