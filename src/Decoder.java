97a1dcb5fd2f690f9bd191843e9ee0e6a75b4cce
joondae <68300257+joondae@users.noreply.github.com>
2020-09-29 01:57:18 -0700

    Update Decoder.java
    created constructor and deleted main method so that this class is compatible with Main.java
    
    changed method decode() so that I could print execution time
    
    got rid of unnecessary imports
    
    updated variable names and comments
    
    created helper methods (originally, all code was in main method only)
    
    used to read codestream into a String (not using BufferedReader) i.e. made decoding very inefficient
    
    fileName was not a class variable
    
    decodedMessage was a String, not StringBuffer/StringBuilder i.e. made decoding very inefficient

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,98 +1,122 @@
 //Maya Doyle and Anneliese Ardizzone uwu
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Decoder{
-	public static void main (String[] args){
-		//Creating variables
-		String encodedFileName = "";
-		String theStringToOutput = "";
-		ArrayList<String> newDictionary = new ArrayList<String>();
+	private final int maxDictionarySize = 512;
+	private static String fileName;
+	
+	public Decoder(String encodedFileName) {
+		fileName = encodedFileName;
+	}
+	
+	public void decode() {
+		long startTime = System.nanoTime();
+		writeToFile(decodeCodestream(initializeDictionary(), catalogueCodestream()));
+		long endTime = System.nanoTime();
+		long duration = endTime - startTime;
 		
-		//Pre-filling the dictionary to make later code easier to understand
-		for (int u = 0; u < 256; u++){
-			newDictionary.add("" + (char)(u));
+		System.out.println("Decoding execution time (milliseconds): " + duration/1000000);
+	}
+	
+	private HashMap<Integer, String> initializeDictionary() {
+		HashMap<Integer, String> dictionary = new HashMap<Integer,String>(maxDictionarySize);
+		
+		for(int i = 0; i < 256; i++) {
+			dictionary.put(i, "" + (char) i);
 		}
-
-		//Getting encoded file name from user
-		Scanner keyboard = new Scanner(System.in);
-    	System.out.print ("Enter file name/path for decoding: ");
-    	encodedFileName = keyboard.nextLine();
-
-    	long startTime = System.nanoTime();
-    	
-    	//Reading in the encoded file's contents to a string
-    	String encodedFileContents = ""; 
-		try{
-			FileReader fileRead = new FileReader (encodedFileName);
-			char currentLetterInEncodedFileContents = (char)(fileRead.read());
-			while ((int)currentLetterInEncodedFileContents < 512){ //Less than 512 because the maximum dictionary length is 512 (meanig the greatest possible number is 511)
-				encodedFileContents += currentLetterInEncodedFileContents;
-				currentLetterInEncodedFileContents = (char)(fileRead.read());
-			}
-			fileRead.close();
-		}
-		catch(IOException ex){
-			System.out.println ("File not found."); 
-		}
-
-
-		//Splitting encodedFileContents into each individual Strings for each number as opposed to one giant String
-		String[] eachNumber = encodedFileContents.split(" ");
-
-		//Converting each of the smaller Strings holding each number from Strings to ints
-		ArrayList<Integer> theEncodedNumbers = new ArrayList<Integer>();
-		for (int k = 0; k < eachNumber.length; k++){
-			theEncodedNumbers.add(Integer.parseInt(eachNumber[k]));
-		}
-
-		//Loop through each encoded value.
-			//If the encodedValue < 256 
-				//theStringToOutput += (char)(theEncodedNumbers.get(index))
-			//If the encodedValue is present in newDictionary
-				//theStringToOutput += newDictionary(theEncodedNumbers.get(index - 256))
-			//Else (if it's not <256 and not present in the dictionary)
-				//theStringToOutput += theEncodedNumbers[index - 3] + theEncodedNumbers[index- 2] (because theEncodedNumbers[i] would be the previous two combined. Are -3 and -2 the right numbers to use?)
-				//newDictionary.add(encodedValues[i])
-			String currentDecodedValue = "";
-			String previousDecodedValue = "";
-			String firstCharOfCurrentDecodedValue;
-			int previousCode;
-			int currentCode;
-
-			//The below code essentially recreates the dictionary and creates the output in the same was you would decode it by hand. It was way harder to write than I thought it would be :')
-			previousCode = theEncodedNumbers.get(0);
-			theStringToOutput += "" + (char)(previousCode);
-
-			for (int k = 1; k < theEncodedNumbers.size(); k++){
-				currentCode = theEncodedNumbers.get(k);
-				previousDecodedValue = "" + (newDictionary.get(previousCode));
-				if (currentCode >= newDictionary.size()){ //This if statements just fixes a weird error that sometimes happens when you have the current code right after itself
-					currentDecodedValue = previousDecodedValue + previousDecodedValue.substring(0,1);
-				}
-				else{
-					currentDecodedValue = "" + (newDictionary.get(currentCode));
-				}
-				theStringToOutput += currentDecodedValue;
-				firstCharOfCurrentDecodedValue = currentDecodedValue.substring(0,1); //there's probably a better way to get the first char but this works and it's fast so ¯\_(ツ)_/¯
-				newDictionary.add(previousDecodedValue + firstCharOfCurrentDecodedValue);
-				previousCode = currentCode;
-			}
-
-		//Print the decoded message and write a file containing the decoded numbers to decodedMessage.txt.
-		System.out.println(theStringToOutput);
-
+		
+		return dictionary;
+	}
+	
+	private ArrayList<Integer> catalogueCodestream() {
+		ArrayList<Integer> codestream = new ArrayList<Integer>();
+		
 		try {
-			FileWriter writeToFile = new FileWriter("decodedMessage.txt");
-			writeToFile.write(theStringToOutput);
-			writeToFile.close();
-		    } 
-		catch (IOException e) {
-			System.out.println("IOException");
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+			
+			String line = "";
+			if((line = br.readLine()) != null) {
+				for(String code : line.split(" ")) {
+					codestream.add(Integer.parseInt(code));
+				}
+			}
+			
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 		
-		long endTime = System.nanoTime(); 
-		System.out.println("Method execution time (milliseconds): " + (endTime - startTime));
+		//For debugging only (check that Decoder's codestream is the same as Encoder's codestream)
+		//System.out.println("codestream size: " + codestream.size());
+		
+		return codestream;
+	}
+	
+	private StringBuffer decodeCodestream(HashMap<Integer, String> dictionary, ArrayList<Integer> codestream) {
+		int dictionarySize = 256;
+		String w = "" + (char) (int) codestream.remove(0);
+		StringBuffer decodedCodestream = new StringBuffer(w);
+		
+		for(int k : codestream) {
+			String entry;
+			
+			if(dictionary.containsKey(k)) {
+				entry = dictionary.get(k);
+			}
+			else if(k == dictionarySize) {
+				entry = w + w.charAt(0);
+			}
+			else {
+				throw new IllegalArgumentException("Bad compressed k: " + k);
+			}
+			
+			decodedCodestream.append(entry);
+			dictionary.put(dictionarySize++, w + entry.charAt(0));
+			w = entry;
+		}
+		
+		return decodedCodestream;
+	}
+	
+	private void writeToFile(StringBuffer decodedCodestream) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("decodedMessage.txt")));
+			
+			bw.write(decodedCodestream.toString());
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean checkDecodedFile(String fileName1, String fileName2) {
+		try {
+			BufferedReader br1 = new BufferedReader(new FileReader(new File(fileName1)));
+			BufferedReader br2 = new BufferedReader(new FileReader(new File(fileName2)));
+			
+			while(br1.ready() && br2.ready()) {
+				if(br1.read() != br2.read()) {
+					br1.close();
+					br2.close();
+					
+					return false;
+				}
+			}
+			
+			br1.close();
+			br2.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
 	}
 }
